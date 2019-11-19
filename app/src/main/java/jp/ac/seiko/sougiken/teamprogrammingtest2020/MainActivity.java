cf7b9a7ebb6255b7cf1109b4597e1e6914fc253f
TOSHI <yakot4594@gmail.com>
2019-11-19 16:41:16 +0900

    Add joined message.

--- app/src/main/java/jp/ac/seiko/sougiken/teamprogrammingtest2020/MainActivity.java
+++ app/src/main/java/jp/ac/seiko/sougiken/teamprogrammingtest2020/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         // host joined.
         // Cyberhac joined.
+        // JellyFish joined.
         setContentView(R.layout.activity_main);
     }
 }
